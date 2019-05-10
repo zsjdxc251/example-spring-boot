d29f171b258ef33eba7d1ab3a40be2aa1a715226
zhengshijun <zhengsj@akulaku.com>
2019-05-10 19:38:30 +0800

    jooq

---
+++ example-spring-boot-docs/example-spring-boot-docs-jooq/src/main/java/com/lesson/boot/docs/jooq/sample/ActivitySample.java
@@ -0,0 +1,12 @@
+package com.lesson.boot.docs.jooq.sample;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/5/9.
+ */
+public class ActivitySample {
+
+	public static void main(String[] args) {
+
+	}
+}
