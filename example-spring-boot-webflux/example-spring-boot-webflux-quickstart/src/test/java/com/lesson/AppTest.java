3177588f4607178b9ba34138fd986c53460e69bc
Stream <zsjdxc251@live.com>
2019-05-16 22:37:04 +0800

    Map转POJO

--- example-spring-boot-webflux/example-spring-boot-webflux-quickstart/src/test/java/com/lesson/AppTest.java
+++ example-spring-boot-webflux/example-spring-boot-webflux-quickstart/src/test/java/com/lesson/AppTest.java
@@ -2,7 +2,16 @@ package com.lesson;
 
 import static org.junit.Assert.assertTrue;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.base.Objects;
+import lombok.Data;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections.MapUtils;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -10,11 +19,34 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     *  Map转POJO
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
+
+        User user = new User();
+        Map<String,String> map = new HashMap<>();
+        map.put("name","1");
+        map.put("age","2");
+        try {
+            BeanUtils.populate(user,map);
+            System.out.println(JSON.toJSONString(user));
+            System.out.println(JSON.toJSONString(map));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    @Data
+    public class User{
+        private String name;
+        private String age;
     }
 }
