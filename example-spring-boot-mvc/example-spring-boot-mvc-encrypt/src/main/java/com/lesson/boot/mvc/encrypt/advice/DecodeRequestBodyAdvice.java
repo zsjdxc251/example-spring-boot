195dd591edd04dcda655782490b81dc70732fb5d
zhengshijun <zhengsj@akulaku.com>
2019-06-05 13:11:16 +0800

    BodyAdvice

---
+++ example-spring-boot-mvc/example-spring-boot-mvc-encrypt/src/main/java/com/lesson/boot/mvc/encrypt/advice/DecodeRequestBodyAdvice.java
@@ -0,0 +1,38 @@
+package com.lesson.boot.mvc.encrypt.advice;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/6/5.
+ */
+@RestControllerAdvice("com.lesson.boot.mvc.encrypt.controller")
+public class DecodeRequestBodyAdvice implements RequestBodyAdvice {
+
+	@Override
+	public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return true;
+	}
+
+	@Override
+	public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+		return inputMessage;
+	}
+
+	@Override
+	public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return body;
+	}
+
+	@Override
+	public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return body;
+	}
+}
