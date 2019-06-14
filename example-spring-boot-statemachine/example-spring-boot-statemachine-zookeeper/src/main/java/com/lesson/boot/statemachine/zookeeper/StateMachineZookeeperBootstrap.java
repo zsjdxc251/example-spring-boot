45365fb1979b129369bd5a54beb136678c1713f3
zhengshijun <zhengsj@akulaku.com>
2019-06-14 12:08:32 +0800

    spring shell

--- example-spring-boot-statemachine/example-spring-boot-statemachine-zookeeper/src/main/java/com/lesson/boot/statemachine/zookeeper/StateMachineZookeeperBootstrap.java
+++ example-spring-boot-statemachine/example-spring-boot-statemachine-zookeeper/src/main/java/com/lesson/boot/statemachine/zookeeper/StateMachineZookeeperBootstrap.java
@@ -1,7 +1,11 @@
 package com.lesson.boot.statemachine.zookeeper;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.jline.PromptProvider;
 
 /**
  * @author zhengshijun
@@ -15,6 +19,11 @@ public class StateMachineZookeeperBootstrap {
 
 		SpringApplication.run(StateMachineZookeeperBootstrap.class, args);
 
+	}
+
+	@Bean
+	public PromptProvider myPromptProvider() {
+		return () -> new AttributedString("spring-shell:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
 	}
 
 
--- example-spring-boot-statemachine/example-spring-boot-statemachine-zookeeper/src/main/java/com/lesson/boot/statemachine/zookeeper/TranslationCommands.java
+++ example-spring-boot-statemachine/example-spring-boot-statemachine-zookeeper/src/main/java/com/lesson/boot/statemachine/zookeeper/TranslationCommands.java
@@ -16,43 +16,16 @@ import java.util.Locale;
  */
 @ShellComponent()
 public class TranslationCommands {
-	@ShellMethod(value = "A command whose name looks the same as another one.", key = "help me out")
-	public void helpMeOut() {
-		System.out.println("You can go");
+
+	@ShellMethod(value = "Add two integers together.",key = "sum")
+	public int add(int a, int b) {
+		return a + b;
+	}
+	@ShellMethod(value = "Add two integers together." ,prefix="-")
+	public int add1(int a, @ShellOption("--c") int b) {
+		System.out.println(a +"-" + b);
+		return a + b;
 	}
 
-	@ShellMethod("Change Password. Shows support for bean validation.")
-	public String changePassword(@Size(min = 8) String password) {
-		return "Password changed";
-	}
 
-	@ShellMethod(value = "Shows non trivial character encoding.")
-	public String helloWorld() {
-		return "こんにちは世界";
-	}
-
-	@ShellMethod("Shows support for boolean parameters, with arity=0.")
-	public void shutdown(@ShellOption(arity = 0) boolean force) {
-		System.out.println("You passed " + force);
-	}
-
-//	@ShellMethod("Test completion of special values.")
-//	public void quote(@ShellOption(valueProvider = FunnyValuesProvider.class) String text) {
-//		System.out.println("You said " + text);
-//	}
-
-	@ShellMethod("Add numbers.")
-	public int add(int a, int b, int c) {
-		return a + b + c;
-	}
-
-	@ShellMethod("Fails with an exception. Shows enum conversion.")
-	public void fail(ElementType elementType) {
-		throw new IllegalArgumentException("You said " + elementType);
-	}
-
-	@ShellMethod("Add array numbers.")
-	public double addDoubles(@ShellOption(arity = 3) double[] numbers) {
-		return Arrays.stream(numbers).sum();
-	}
 }
