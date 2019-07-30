f4db3ce701a59edf017345b0c873aff2cca3fe1f
Stream <zsjdxc251@live.com>
2019-07-31 00:00:30 +0800

    docker

---
+++ example-spring-boot-docs/example-spring-boot-docs-docker/src/main/java/com/lesson/boot/docs/docker/DocsDockerBootstrap.java
@@ -0,0 +1,16 @@
+package com.lesson.boot.docs.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengshijun
+ * @version created on 2019/7/30.
+ */
+@SpringBootApplication
+public class DocsDockerBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsDockerBootstrap.class,args);
+    }
+}
