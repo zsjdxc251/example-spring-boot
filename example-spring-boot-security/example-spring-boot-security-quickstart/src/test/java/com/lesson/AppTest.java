627ee77fc40a4105338e292815fb23b36fc6f2da
zhengshijun <zhengsj@akulaku.com>
2020-05-26 13:13:06 +0800

    spring boot 2.3 特效

--- example-spring-boot-security/example-spring-boot-security-quickstart/src/test/java/com/lesson/AppTest.java
+++ example-spring-boot-security/example-spring-boot-security-quickstart/src/test/java/com/lesson/AppTest.java
@@ -2,7 +2,16 @@ package com.lesson;
 
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.http.server.PathContainer;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -15,6 +24,35 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        PathPatternParser pathPatternParser = new PathPatternParser();
+        PathPattern pathPattern =  pathPatternParser.parse("/**/order/**");
+//        boolean result = pathPattern.matches(new PathContainer(){
+//
+//            @Override
+//            public String value() {
+//                return null;
+//            }
+//
+//            @Override
+//            public List<Element> elements() {
+//                return ImmutableList.of(()->"/api/app/order/invest/*");
+//            }
+//        });
+
+        boolean result = pathPattern.matches(PathContainer.parsePath("/api/app/order/invest/3"));
+
+        System.out.println(result);
+
+        //AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+
+
+    }
+
+    @Test
+    public void test1(){
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+        System.out.println(antPathMatcher.match("/**/order/**","/api/app/order/invest/3"));
     }
 }
