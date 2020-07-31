fd5054e5e5f2e5dd2b2e614b734de065a60e2191
zhengshijun <zhengsj@akulaku.com>
2020-07-31 19:49:22 +0800

    添加 sharding

---
+++ example-spring-boot-sharding/src/main/java/com/lesson/boot/sharding/ShardingBootstrap.java
@@ -0,0 +1,11 @@
+package com.lesson.boot.sharding;
+
+/**
+ * @author zhengshijun
+ * @version created on 2020/7/29.
+ */
+public class ShardingBootstrap {
+	public static void main(String[] args) {
+
+	}
+}
